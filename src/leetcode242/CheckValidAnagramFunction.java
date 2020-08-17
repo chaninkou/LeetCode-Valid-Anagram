7f6a5eb9f8c0913186e99d56a1c75c076ce1312d
chaninkou <michaelkoulove@yahoo.com>
2020-08-16 19:39:01 -0700

    Redid problem with 3 different ways, using two hashmap, or int[26].

--- src/leetcode242/CheckValidAnagramFunction.java
+++ src/leetcode242/CheckValidAnagramFunction.java
@@ -1,4 +1,4 @@
-package anagram;
+package leetcode242;
 
 import java.util.HashMap;
 import java.util.Map;
--- src/leetcode242/Main.java
+++ src/leetcode242/Main.java
@@ -1,4 +1,4 @@
-package anagram;
+package leetcode242;
 
 public class Main {
 	public static void main(String[] args) {
